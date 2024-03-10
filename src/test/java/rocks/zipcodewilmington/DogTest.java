801ca3cfcc80efc585799a4326137a9e0df9d3df
LydiaStoneKonstanski <yourname@example.com>
2024-03-09 22:25:47 -0500

    Dog tests done

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.Mammal;
-import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
 import java.util.Date;
 
@@ -18,18 +17,30 @@ public class DogTest {
     // TODO - XCreate tests for `new Dog(String name, Date birthDate, Integer id)`
     // TODO - XCreate tests for `speak`
     // TODO - XCreate tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
     // TODO - XCreate tests for `Integer getId()`
     // TODO - XCreate test to check Animal inheritance; google search `java instanceof keyword`
     // TODO - XCreate test to check Mammal inheritance; google search `java instanceof keyword`
-    @Test
-    public void isNewDogtest() {
-        String name = "Argyl";
-        Date birthDate = new Date (1994, 6, 30);
-        Integer id = 356;
 
-        Animal pet = AnimalFactory.createDog(name, birthDate, id);
-        assertTrue(pet instanceof Dog);
+    // TODO - Create tests for `void eat(Food food)`
+    @Test
+    public void dogConstructorTest() {
+        // Given (dog data)
+        String givenName = "Andy";
+        Date givenBirthDate = new Date(2003, 6, 16);
+        Integer givenId = 7456;
+
+        // When (a dog is constructed)
+        Dog dog = new Dog(givenName, givenBirthDate, givenId);
+
+        // When (we retrieve data from the cat)
+        String retrievedName = dog.getName();
+        Date retrievedBirthDate = dog.getBirthDate();
+        Integer retrievedId = dog.getId();
+
+        // Then (we expect the given data, to match the retrieved data)
+        Assert.assertEquals(givenName, retrievedName);
+        Assert.assertEquals(givenBirthDate, retrievedBirthDate);
+        Assert.assertEquals(givenId, retrievedId);
     }
     @Test
     public void setNameTest() {
@@ -46,9 +57,9 @@ public class DogTest {
     }
     @Test
     public void dogSpeaksTest() {
-        Mammal pet = new Dog(null, null, null);
+        Dog dog = new Dog(null, null, null);
         String expected = "bark!";
-        String actual = pet.speak();
+        String actual = dog.speak();
         Assert.assertEquals(expected, actual);
     }
     @Test
@@ -56,9 +67,8 @@ public class DogTest {
         String name = null;
         Date birthDate = new Date(2019, 9, 18);
         Integer id = null;
-
-        Mammal pet = AnimalFactory.createDog(name, birthDate, id);
-        Assert.assertEquals(birthDate, pet.getBirthDate()); //Check birthDate is set
+        Dog dog = new Dog(name, birthDate, id);
+        Assert.assertEquals(birthDate, dog.getBirthDate());
     }
     @Test
     public void dogGetsNewIdTest() {
@@ -66,8 +76,8 @@ public class DogTest {
         Date birthDate = null;
         Integer id = 1903;
 
-        Mammal pet = AnimalFactory.createDog(name, birthDate, id);
-        Assert.assertEquals(id, pet.getId()); //Check birthDate is set
+        Dog dog = new Dog(name, birthDate, id);
+        Assert.assertEquals(id, dog.getId()); //Check birthDate is set
     }
     @Test
     public void DogIsInstanceOfAnimalTest() {
@@ -75,8 +85,8 @@ public class DogTest {
         Date birthDate = null;
         Integer id = null;
 
-        Animal pet = AnimalFactory.createDog(name, birthDate, id);
-        assertTrue(pet instanceof Animal);
+        Dog dog = new Dog(name, birthDate, id);
+        assertTrue(dog instanceof Animal);
     }
     @Test
     public void DogIsInstanceOfMammalTest() {
@@ -84,7 +94,7 @@ public class DogTest {
         Date birthDate = null;
         Integer id = null;
 
-        Animal pet = AnimalFactory.createDog(name, birthDate, id);
-        assertTrue(pet instanceof Mammal);
+        Dog dog = new Dog(name, birthDate, id);
+        assertTrue(dog instanceof Mammal);
     }
 }
